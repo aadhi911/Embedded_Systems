a27fa35152230a053217fbdeb7e01371700ae581
Aditya <adithyareddy655@gmail.com>
2021-09-13 17:28:29 -0400

    Structure is defined and added with Node loop

--- data.c
+++ data.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typdef struct{
+	int data;
+	Node* next;
+}Node;
+void printList(Node* head){
 
+	while(head != NULL){
+	printf("%d\r\n", head->data);
+	head = head->data;
+	}	
+}
 
 int main(void){
 
